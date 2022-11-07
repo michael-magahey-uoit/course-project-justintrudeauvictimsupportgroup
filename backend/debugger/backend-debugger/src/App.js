5085453a490201844b6a9891bb66e21be1b972e7
DeadmanLabs <michael.magahey@gmail.com>
2022-11-07 16:19:19 -0500

    Basic Debugging Site

--- backend/debugger/backend-debugger/src/App.js
+++ backend/debugger/backend-debugger/src/App.js
@@ -1,10 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 
 function App() {
+  const [socket, setSocket] = useState(null);
+  useEffect(() => {
+    const connection = io(`https://127.0.0.1:9110`);
+    connection.on('status', (details) => {
+      alert(details);
+    });
+    connection.on('queue', (details) => {
+      alert(details);
+    });
+    setSocket(connection);
+    return () => {
+      connection.close();
+    }
+  }, [setSocket]);
+
+
+
+
   return (
     <div className="App">
-      
+      { socket ? 
+      (
+        <>
+          <div className="game">
+            <div className="upArrow" onMouseEnter={() => socket.emit("up")} onMouseLeave={() => socket.emit("clear")} />
+            <div className="leftArrow inline" onMouseEnter={() => socket.emit("left")} onMouseLeave={() => socket.emit("clear")} />
+            <div className="rightArrow inline" onMouseEnter={() => socket.emit("right")} onMouseLeave={() => socket.emit("clear")} />
+            <div className="downArrow" onMouseEnter={() => socket.emit("down")} onMouseLeave={() => socket.emit("clear")} />
+            <div className="dropButton" onMouseEnter={() => socket.emit("drop")} onMouseLeave={() => socket.emit("clear")} /> 
+          </div>
+        </>
+      ) : (
+        <>
+          <div>Not Connected!</div>
+        </>
+      )}
     </div>
   );
 }
