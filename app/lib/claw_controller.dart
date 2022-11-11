0dbce3104984cbb2e5d7d7418fd06faee1da7ce3
KeithMiddleton <keith.middleton@ontariotechu.net>
2022-11-10 22:00:15 -0500

    Added temp video

--- app/lib/claw_controller.dart
+++ app/lib/claw_controller.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class ClawController extends StatefulWidget {
   ClawController({Key? key, this.title}) : super(key: key);
@@ -10,20 +11,87 @@ class ClawController extends StatefulWidget {
 }
 
 class _ClawControllerState extends State<ClawController> {
+  late YoutubePlayerController controller;
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title!),
-      ),
-      body: _buildClawController(),
+  void initState(){
+    super.initState();
+
+    const url = 'https://www.youtube.com/watch?v=jfKfPfyJRdk';
+
+    controller = YoutubePlayerController(
+      initialVideoId: YoutubePlayer.convertUrlToId(url)!,
+      flags: const YoutubePlayerFlags(
+        autoPlay: true,
+      )
     );
   }
 
-  Widget _buildClawController() {
-    return Row(
+  @override
+  void deactivate() {
+    controller.pause();
+
+    super.deactivate();
+  }
+
+  @override
+  void dispose(){
+    controller.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) =>
+      YoutubePlayerBuilder(
+        player: YoutubePlayer(
+          controller: controller,
+        ),
+        builder: (context, player) =>
+            Scaffold(
+              appBar: AppBar(
+                title: Text(widget.title!),
+              ),
+              body: _buildClawController(player),
+            ),
+      );
+
+  Widget _buildClawController(Widget player) {
+    double width = MediaQuery.of(context).size.width;
+    return Column(
       children: [
-        Row()
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: width/1.1,
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: player,
+            )
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: width/1.2,
+              height: 40,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(width/2)
+              ),
+              child: const Center(
+                child: Text(
+                    "You are # out of # players!",
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white
+                    )
+                ),
+              )
+            )
+          ],
+        )
       ],
     );
   }
