e1da4b090d52cbf826963feb966c8eb0c51eee80
Haaris Ahmed <haarisahmed09@gmail.com>
2022-11-10 23:29:55 -0500

    another map update

--- app/lib/frontend/geo_location/location_page/locations_page.dart
+++ app/lib/frontend/geo_location/location_page/locations_page.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/widgets.dart'
+
+class  extends StatefulWidget {
+      const ({Key? key}) : super(key: key);
+
+      @override
+      State<> createState() => _State();
+    }
+
+    class _State extends State<> {
+      @override
+      Widget build(BuildContext context) {
+        return Container();
+      }
+}
