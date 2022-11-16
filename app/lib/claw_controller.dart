a76734444acf625c9483558c62cffd8270c815c3
KeithMiddleton <keith.middleton@ontariotechu.net>
2022-11-16 15:48:58 -0500

    Final UI layout

--- app/lib/claw_controller.dart
+++ app/lib/claw_controller.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
+import 'claw_movement.dart';
+
 class ClawController extends StatefulWidget {
   ClawController({Key? key, this.title}) : super(key: key);
 
@@ -11,41 +13,33 @@ class ClawController extends StatefulWidget {
 }
 
 class _ClawControllerState extends State<ClawController> {
-  late YoutubePlayerController controller;
+  late YoutubePlayerController videoPlayer;
+  final _claw = ClawMovement();
 
   @override
   void initState(){
     super.initState();
 
+    //TODO: Put actual url in
+    //url of claw machine stream
     const url = 'https://www.youtube.com/watch?v=jfKfPfyJRdk';
 
-    controller = YoutubePlayerController(
+    //Create the youtube player and set it to auto play
+    //when loaded and give it livestream attributes
+    videoPlayer = YoutubePlayerController(
       initialVideoId: YoutubePlayer.convertUrlToId(url)!,
       flags: const YoutubePlayerFlags(
         autoPlay: true,
+        isLive: true
       )
     );
-  }
-
-  @override
-  void deactivate() {
-    controller.pause();
-
-    super.deactivate();
-  }
-
-  @override
-  void dispose(){
-    controller.dispose();
-
-    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) =>
       YoutubePlayerBuilder(
         player: YoutubePlayer(
-          controller: controller,
+          controller: videoPlayer,
         ),
         builder: (context, player) =>
             Scaffold(
@@ -57,24 +51,35 @@ class _ClawControllerState extends State<ClawController> {
       );
 
   Widget _buildClawController(Widget player) {
-    double width = MediaQuery.of(context).size.width;
+    double _width = MediaQuery.of(context).size.width; //Width of device
+    double _height = MediaQuery.of(context).size.height; //Height of device
+    double _buttonSize = 50.0; //Size of controller buttons
+    double _buttonPadding = 5.0; //Size of button padding
+
     return Column(
       children: [
+        //Puts the video player at the top of the column
+        //and gives some padding
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
-              width: width/1.1,
-              padding: const EdgeInsets.symmetric(vertical: 20),
+              width: _width/1.1,
+              padding: EdgeInsets.only(top: _height/36, bottom: _height/7),
               child: player,
             )
           ],
         ),
+        //Button arrangement, when clicked calls a function from claw_movement
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             IconButton(
-                onPressed: (){},
+              iconSize: _buttonSize,
+                padding: EdgeInsets.only(bottom: _buttonPadding),
+                onPressed: (){
+                  _claw.moveForward();
+                },
                 icon: const Icon(Icons.keyboard_arrow_up_rounded)
             )
           ],
@@ -83,15 +88,26 @@ class _ClawControllerState extends State<ClawController> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             IconButton(
-                onPressed: (){},
+                iconSize: _buttonSize,
+                padding: EdgeInsets.only(right: _buttonPadding),
+                onPressed: (){
+                  _claw.moveLeft();
+                },
                 icon: const Icon(Icons.keyboard_arrow_left_rounded)
             ),
             IconButton(
-                onPressed: (){},
+                iconSize: _buttonSize,
+                onPressed: (){
+                  _claw.drop();
+                },//_claw.drop(),
                 icon: const Icon(Icons.circle)
             ),
             IconButton(
-                onPressed: (){},
+                iconSize: _buttonSize,
+                padding: EdgeInsets.only(left: _buttonPadding),
+                onPressed: (){
+                  _claw.moveRight();
+                },
                 icon: const Icon(Icons.keyboard_arrow_right_rounded)
             ),
           ],
@@ -100,30 +116,39 @@ class _ClawControllerState extends State<ClawController> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             IconButton(
-                onPressed: (){},
+                iconSize: _buttonSize,
+                padding: EdgeInsets.only(top: _buttonPadding),
+                onPressed: (){
+                  _claw.moveBackwards();
+                },
                 icon: const Icon(Icons.keyboard_arrow_down_rounded)
             )
           ],
         ),
+        //Text box that tells the user how many people are ahead of them
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-              width: width/1.2,
-              height: 40,
-              decoration: BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.circular(width/2)
-              ),
-              child: const Center(
-                child: Text(
-                    "You are # out of # players!",
-                    style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.white
-                    )
+            Padding(
+              padding: EdgeInsets.only(top: _height/6),
+              child: Container(
+                width: _width/1.2,
+                height: 40,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(_width/2)
                 ),
-              )
+                child: const Center(
+                  child: Text(
+                    //TODO: Replace # with number
+                      "You are # out of # players!",
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white
+                      )
+                  ),
+                )
+              ),
             )
           ],
         )
---
+++ app/lib/claw_movement.dart
@@ -0,0 +1,22 @@
+//TODO: add functionality
+class ClawMovement{
+  moveForward(){
+    print("Foward"); //Debugging, remove later
+  }
+
+  moveBackwards(){
+    print("Backward"); //Debugging, remove later
+  }
+
+  moveRight(){
+    print("Right"); //Debugging, remove later
+  }
+
+  moveLeft(){
+    print("Left"); //Debugging, remove later
+  }
+
+  drop(){
+    print("Drop"); //Debugging, remove later
+  }
+}
